a5cc09336b933cfbeacd09ff2c9625319b510ce6
Harshal Baswant <harshalbaswant44193@gmail.com>
2023-05-04 21:16:48 +0530

    first commit

---
+++ Practice 1/index.js
@@ -0,0 +1,167 @@
+// console.log("Hello world");
+// Values and variables in javascript
+
+// var myName = 'Harshal Baswant';
+// console.log(myName)
+
+
+
+// var - key , myname=value , Harshal Baswant - value
+// myAge- Camelcase
+
+// var myAge=25
+// console.log(myAge)
+
+// the first character must be letter, $ , undersore. you cant use number as the first character.
+
+// DATATYPES IN JS
+
+// console.log("type of myAge is :",typeof(myAge))
+// console.log("type of myName is :",typeof(myName))
+
+
+// var iAmHarshal=true
+// console.log(iAmHarshal,typeof(iAmHarshal))
+
+
+
+
+// console.log(10+"20");
+
+// console.log(10-"20");
+
+
+
+// javascript variables
+
+// variables -  it is container to store values 
+
+// var num1 = 34;
+// var num2 = 56;
+// console.log("sum of num1 and num2 - ",num1+num2);
+
+// datatypes in js
+
+// numbers
+// var num1 = 34;
+// var num2 = 56;
+
+// string
+// var str1="This is string ";
+// var str2='this is also string';
+
+// objects
+
+
+// var marks = {
+
+//     ravi :34,
+//     shubham:30,
+//     harshal:99
+// }
+
+// console.log(marks);
+
+// Booleans
+
+// var a=true;
+// var b=false;
+// console.log(a,b);
+
+// 
+
+// var und=undefined;
+// var r;
+// console.log(und,r)
+// 
+
+// var t= null
+// console.log(t)
+
+/* at a very high level there are two types of datatypes 
+// 1 primitive 
+undefined null numbers string boolean symbol
+2 refrence
+arrays  
+objects  */
+
+
+// arrays
+
+// var arr= [1,2,3,4,5,"harshal"]
+
+// console.log(arr[0])
+
+// oprators in js 
+// arithmetic 
+var a=100;
+var b=10;
+e=a+b;
+console.log("The value of a+b is ",a+b );
+console.log("The value of a-b is ",a-b );
+console.log("The value of a*b is ",a*b );
+console.log("The value of a/b is ",a/b );
+
+// assigment op
+
+var c=b
+c+=2
+c-=2 //c=c-2
+c*=2
+c/=2
+
+console.log(c)
+
+
+// comparison
+
+
+var x=34
+var y=56
+// console.log(x==y)
+// console.log(x>=y)
+// console.log(x<=y)
+
+// console.log(x>y)
+// console.log(x<y)
+
+
+// logical
+
+// && and
+
+// console.log(true && true)
+// console.log(true && false)
+// console.log(false && true)
+// console.log(false && false)
+
+// or ||
+// console.log(true || true)
+// console.log(true || false)
+// console.log(false || true)
+// console.log(false || false)
+
+// not !
+
+// console.log(!false)
+// console.log(!true)
+
+ function avg(a,b){
+    return (a+b)/2;
+ }
+c1=avg(4,6);
+c2=avg(14,16);
+console.log("average - ",c1,c2)
+
+
+
+
+
+
+
+
+
+
+
+
+
